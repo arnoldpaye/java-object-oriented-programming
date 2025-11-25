fbbc166d34211f1b9b2b7561a298ee598b1529cc
Arnold Paye <arnoldpayet@gmail.com>
2025-11-25 08:41:02 -0400

    runtime polymorphism

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,12 +1,19 @@
+import java.util.ArrayList;
+
 public class Main {
   public static void main(String[] args) {
-    ModArrayList<Integer> modList = new ModArrayList<>();
-    modList.add(1);
-    modList.add(56);
-    modList.add(13);
-    modList.add(22);
-    System.out.println(modList.getUsingMod(5));
-    System.out.println(modList.getUsingMod(-2));
-    System.out.println(modList.getUsingMod(33));
+    OddArrayList oddArrayList = new OddArrayList(1, 2, 3, 4, 5, 6, 7, 9, -13);
+
+    System.out.println(oddArrayList);
+
+    ArrayList<Integer> list = new ArrayList<>();
+    list.add(1);
+    list.add(2);
+    list.add(3);
+    list.add(4);
+    list.add(7);
+    list.add(9);
+    list.add(-13);
+    System.out.println(list);
   }
 }
