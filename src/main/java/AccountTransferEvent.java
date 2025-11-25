e3618180d2f50f6d43585f33888766435ea2bcad
Arnold Paye <arnoldpayet@gmail.com>
2025-11-25 11:53:29 -0400

    using interfaces

---
+++ src/main/java/AccountTransferEvent.java
@@ -0,0 +1,21 @@
+import java.sql.Timestamp;
+
+public class AccountTransferEvent implements Event {
+  private final Long createdTimestamp;
+  private final String id;
+
+  public AccountTransferEvent(String id) {
+    this.createdTimestamp = new Timestamp(System.currentTimeMillis()).getTime();
+    this.id = id;
+  }
+
+  @Override
+  public Long getTimeStamp() {
+    return this.createdTimestamp;
+  }
+
+  @Override
+  public void process() {
+    System.out.println("Customer " + id + " needs to transfer their service. " + "Reaching out to CTE to remove service from old device and add service to new device.");
+  }
+}
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,10 +1,16 @@
-import java.io.IOException;
-
 public class Main {
 
-  public static void main(String[] args) throws IOException {
-    DigitsOnlyFileReader digitsOnlyFileReader = new DigitsOnlyFileReader("message.txt");
-    System.out.println(digitsOnlyFileReader.readFile());
-    System.out.println(digitsOnlyFileReader.getPath());
+  public static void main(String[] args) {
+    PasswordChangeEvent eventOne = new PasswordChangeEvent("321928399");
+    AccountTransferEvent eventTwo = new AccountTransferEvent("1936428194");
+    MissedPaymentEvent eventThree = new MissedPaymentEvent("93827451");
+
+    Event[] events = {eventOne, eventTwo, eventThree};
+
+    for (Event e : events) {
+      System.out.println(e.getTimeStamp());
+      e.process();
+      System.out.println();
+    }
   }
 }
